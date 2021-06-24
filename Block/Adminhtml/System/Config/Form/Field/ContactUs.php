a55e55b3b218cdfbcb032505e9d31bb3d462d7b5
Nabil SEMAOUNE <nabil.semaoune@lyra-network.com>
2021-06-24 17:50:01 +0200

    [BOT] Migrate hotfix 2.5.7 to GitHub.

---
+++ Block/Adminhtml/System/Config/Form/Field/ContactUs.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Copyright © Lyra Network.
+ * This file is part of PayZen plugin for Magento 2. See COPYING.md for license details.
+ *
+ * @author    Lyra Network (https://www.lyra.com/)
+ * @copyright Lyra Network
+ * @license   https://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ */
+namespace Lyranetwork\Payzen\Block\Adminhtml\System\Config\Form\Field;
+
+/**
+ * Custom renderer for the contact us element.
+ */
+class ContactUs extends \Magento\Config\Block\System\Config\Form\Field
+{
+    /**
+     * Unset some non-related element parameters.
+     *
+     * @param \Magento\Framework\Data\Form\Element\AbstractElement $element
+     * @return string
+     */
+    public function render(\Magento\Framework\Data\Form\Element\AbstractElement $element)
+    {
+        $comment = \Lyranetwork\Payzen\Model\Api\PayzenApi::formatSupportEmails('support@payzen.eu');
+        $element->setComment($comment);
+
+        return parent::render($element);
+    }
+}
